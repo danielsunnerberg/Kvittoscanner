afd69667043dd155817d972758b097b5b0c32ec4
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2017-02-01 09:25:11 +0100

    Basis for OpenCV-support

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import org.opencv.core.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to OpenCV " + Core.VERSION);
+        System.out.println(System.getProperty("java.library.path"));
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat m  = Mat.eye(3, 3, CvType.CV_8UC1);
+        System.out.println("m = " + m.dump());
+    }
+}
