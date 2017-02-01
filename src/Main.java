9d678748e99a622578f1dcd4d3cc21c75e3e0965
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2017-02-01 16:43:29 +0100

    Initialize OpenCV correctly

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import org.opencv.core.*;
 import utilities.ComputerVision;
+import utilities.VideoSplitter;
 
 import java.io.IOException;
 
@@ -7,14 +8,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to OpenCV " + Core.VERSION);
-        System.load(System.getProperty("java.library.path"));
-        //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         Mat m  = Mat.eye(3, 3, CvType.CV_8UC1);
         System.out.println("m = " + m.dump());
-
-        //String response = ComputerVision.executePost("{\"url\":\"http://support.quadriga.nu/hc/sv/article_attachments/200038862/Kassa_-_Plus_och_minus_-_12.PNG\"}");
-        //System.out.println(response);
-
-
     }
 }
