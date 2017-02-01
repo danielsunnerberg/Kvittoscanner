4ec2410dc1e143fb84fe06b43e69af5df91bc6c8
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2017-02-01 16:44:26 +0100

    Add test for video splitter

---
+++ src/test/java/utilities/VideoSplitterTest.java
@@ -0,0 +1,41 @@
+package utilities;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
+import org.opencv.videoio.Videoio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+class VideoSplitterTest {
+
+    @BeforeEach
+    void setUp() {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    @Test
+    void getFramesFromString() {
+        String filePath = VideoSplitterTest.class.getResource("/videos/example-video.mp4").getFile().substring(1);
+        final VideoCapture capture = new VideoCapture(filePath);
+        final int frameCount = (int) capture.get(Videoio.CAP_PROP_FRAME_COUNT);
+
+        assertTrue(capture.isOpened(), "Capture has to be opened. Ensure OpenCV is correctly installed.");
+        assertTrue(frameCount > 0, "Loaded file should have frames");
+
+        List<Mat> frames = VideoSplitter.getFrames(filePath, 0);
+        assertEquals(frameCount, frames.size(), "When requiredFrames=0 all frames should be extracted");
+
+        frames = VideoSplitter.getFrames(filePath, frameCount - 1);
+        assertEquals(frameCount - 1, frames.size(), "Should only extract as many frames as required");
+
+        frames = VideoSplitter.getFrames(filePath, frameCount + 1);
+        assertEquals(frameCount, frames.size(), "Should extract all frames if too many are requested");
+
+        capture.release();
+    }
+}
