626f27cf2fdef51fcf189a5b6fc44cd21e71d69f
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2017-02-01 09:33:36 +0100

    Load OpenCV from correct location

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to OpenCV " + Core.VERSION);
-        System.out.println(System.getProperty("java.library.path"));
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        System.load(System.getProperty("java.library.path"));
+        //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         Mat m  = Mat.eye(3, 3, CvType.CV_8UC1);
         System.out.println("m = " + m.dump());
     }
