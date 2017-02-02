4585260542d21c6b2678d9513da2d50c6112734b
gurrabz <gustav_m_o_bergstrom@hotmail.com>
2017-02-02 16:28:15 +0100

    Add functionality for creating a two tone image

--- src/twotone/TwoTone.java
+++ src/twotone/TwoTone.java
@@ -1,9 +1,6 @@
 package twotone;
 
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfByte;
-import org.opencv.core.Size;
+import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
@@ -27,20 +24,50 @@ public class TwoTone {
 		System.load(System.getProperty("java.library.path"));
 		//System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-		//String file = "/Users/gustavbergstrom/Documents/Kandidat/house.jpeg";
-		String file = "src/test/resources/randomimages/house.jpeg";
+		String file = "src/test/resources/randomimages/ReceiptSwiss.jpeg";
 
-		BufferedImage img = null;
-		try {
-			img=ImageIO.read(new File(file));
-		} catch (IOException e) {
-			e.printStackTrace();
+//		BufferedImage img = null;
+//		try {
+//			img=ImageIO.read(new File(file));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		displayImage(img);
+
+		Mat image = Imgcodecs.imread(file);
+		image = resize(image, 600, 600);
+		displayImage(matToImage(image), "Original");
+
+		Mat hsvImage = new Mat();
+		Imgproc.cvtColor(image, hsvImage, Imgproc.COLOR_BGR2HSV);
+		displayImage(matToImage(hsvImage), "HSV");
+		Core.inRange(hsvImage, new Scalar(0,0,100), new Scalar (255,255,255), hsvImage);
+		displayImage(matToImage(hsvImage), "In range HSV");
+
+		Core.inRange(image, new Scalar(220,220,220), new Scalar (255,255,255), image);
+		displayImage(matToImage(image), "In range original");
+	}
+
+	public static Mat resize (Mat image, int height, int width) {
+
+		Mat newImage = new Mat();
+		int newWidth;
+		int newHeight;
+
+		if (image.height() > height && image.height() > image.width()) {
+			newHeight = height;
+			double ratio = (double) newHeight / image.height();
+			newWidth = (int) (image.width() * ratio);
+		} else if (image.width() > width) {
+			newWidth = width;
+			double ratio = (double) newWidth / image.width();
+			newHeight = (int) (image.width() * ratio);
+		} else {
+			return image;
 		}
-		displayImage(img);
 
-		Mat imageMat = Imgcodecs.imread(file);
-		displayImage(matToImage(imageMat));
-
+		Imgproc.resize(image, newImage, new Size(newWidth, newHeight));
+		return newImage;
 	}
 
 	public static BufferedImage matToImage(Mat m){
@@ -57,11 +84,12 @@ public class TwoTone {
 		return image;
 	}
 
-	public static void displayImage(Image img2) {
-		ImageIcon icon=new ImageIcon(img2);
+	public static void displayImage(Image image, String name) {
+		ImageIcon icon=new ImageIcon(image);
 		JFrame frame=new JFrame();
+		frame.setTitle(name);
 		frame.setLayout(new FlowLayout());
-		frame.setSize(img2.getWidth(null)+50, img2.getHeight(null)+50);
+		frame.setSize(image.getWidth(null)+50, image.getHeight(null)+50);
 		JLabel lbl=new JLabel();
 		lbl.setIcon(icon);
 		frame.add(lbl);
