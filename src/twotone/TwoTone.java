b9cc0d54dcf176cf8466147f01f98c59c7562d0c
gurrabz <gustav_m_o_bergstrom@hotmail.com>
2017-02-03 11:39:23 +0100

    Add method for saving image to disk

--- src/twotone/TwoTone.java
+++ src/twotone/TwoTone.java
@@ -3,52 +3,68 @@ package twotone;
 import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.videoio.VideoCapture;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 
 /**
  * Created by gustavbergstrom on 2017-02-01.
  */
 public class TwoTone {
+
 	public static void main(String args[]) {
 
 		System.load(System.getProperty("java.library.path"));
 		//System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-		String file = "src/test/resources/randomimages/ReceiptSwiss.jpeg";
-
-//		BufferedImage img = null;
-//		try {
-//			img=ImageIO.read(new File(file));
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//		displayImage(img);
+		String imagePath = "src/test/resources/randomimages/";
+		String imageName = "ReceiptSwiss.jpeg";
+		String file = imagePath + imageName;
 
 		Mat image = Imgcodecs.imread(file);
 		image = resize(image, 600, 600);
 		displayImage(matToImage(image), "Original");
 
-		Mat hsvImage = new Mat();
-		Imgproc.cvtColor(image, hsvImage, Imgproc.COLOR_BGR2HSV);
-		displayImage(matToImage(hsvImage), "HSV");
-		Core.inRange(hsvImage, new Scalar(0,0,100), new Scalar (255,255,255), hsvImage);
-		displayImage(matToImage(hsvImage), "In range HSV");
+//		Mat hsvImage = new Mat();
+//		Imgproc.cvtColor(image, hsvImage, Imgproc.COLOR_BGR2HSV);
+//		displayImage(matToImage(hsvImage), "HSV");
+//		Core.inRange(hsvImage, new Scalar(0,0,100), new Scalar (255,255,255), hsvImage);
+//		displayImage(matToImage(hsvImage), "In range HSV");
 
-		Core.inRange(image, new Scalar(220,220,220), new Scalar (255,255,255), image);
-		displayImage(matToImage(image), "In range original");
+		int v0 = 200;
+		int v1 = 200;
+		int v2 = 200;
+		Core.inRange(image, new Scalar(v0,v1,v2), new Scalar (255,255,255), image);
+		BufferedImage processedImage = matToImage(image);
+		saveImage(processedImage, imageName.substring(0, imageName.indexOf('.')),
+				v0 + "_" + v1 + "_" + v2);
+		displayImage(processedImage, "In range original");
 	}
 
-	public static Mat resize (Mat image, int height, int width) {
+	// Save an image to disk
+	public static void saveImage (BufferedImage image, String dirName, String name) {
+		String path = "/Users/gustavbergstrom/Documents/Kandidat/Output/" + dirName;
+		File dir = new File(path);
+		if (!dir.exists()) {
+			dir.mkdir();
+		}
+
+		try {
+			File outputFile = new File(
+					path + "/" + name + ".png");
+			ImageIO.write(image, "png", outputFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// Resize a Mat to fit the specified size
+	public static Mat resize (Mat image, int width, int height) {
 
 		Mat newImage = new Mat();
 		int newWidth;
@@ -70,32 +86,33 @@ public class TwoTone {
 		return newImage;
 	}
 
-	public static BufferedImage matToImage(Mat m){
+	// Convert a Mat to a BufferedImage
+	public static BufferedImage matToImage(Mat mat){
 		int type = BufferedImage.TYPE_BYTE_GRAY;
-		if ( m.channels() > 1 ) {
+		if (mat.channels() > 1) {
 			type = BufferedImage.TYPE_3BYTE_BGR;
 		}
-		int bufferSize = m.channels()*m.cols()*m.rows();
-		byte [] b = new byte[bufferSize];
-		m.get(0,0,b); // get all the pixels
-		BufferedImage image = new BufferedImage(m.cols(),m.rows(), type);
+		int bufferSize = mat.channels() * mat.cols() * mat.rows();
+		byte[] b = new byte[bufferSize];
+		mat.get(0,0, b); // get all the pixels
+		BufferedImage image = new BufferedImage(mat.cols(), mat.rows(), type);
 		final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
 		System.arraycopy(b, 0, targetPixels, 0, b.length);
 		return image;
 	}
 
-	public static void displayImage(Image image, String name) {
-		ImageIcon icon=new ImageIcon(image);
-		JFrame frame=new JFrame();
+	// Display an image on the screen
+	public static void displayImage (Image image, String name) {
+		ImageIcon icon = new ImageIcon(image);
+		JFrame frame = new JFrame();
 		frame.setTitle(name);
 		frame.setLayout(new FlowLayout());
-		frame.setSize(image.getWidth(null)+50, image.getHeight(null)+50);
-		JLabel lbl=new JLabel();
+		frame.setSize(image.getWidth(null) + 50, image.getHeight(null) + 50);
+		JLabel lbl = new JLabel();
 		lbl.setIcon(icon);
 		frame.add(lbl);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-
 
 }
