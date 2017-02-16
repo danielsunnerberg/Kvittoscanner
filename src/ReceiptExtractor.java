4ac8463db2b257b15c887dbee9a698047b7201ed
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2017-02-16 17:05:44 +0100

    Add basis for ReceiptExtractor

---
+++ src/ReceiptExtractor.java
@@ -0,0 +1,42 @@
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.videoio.VideoCapture;
+import utilities.BlurDetector;
+import utilities.EdgeDetector;
+import utilities.VideoSplitter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReceiptExtractor {
+
+    // @todo Set these constants based on the video itself?
+    private final static int NUM_SPLITTED_FRAMES = 50;
+    private final static int NUM_IMAGE_PIECES = 2;
+
+    private EdgeDetector edgeDetector = new EdgeDetector();
+
+    public Mat extractReceipt(VideoCapture source) {
+
+        // Split the video to frames and select the best frames.
+        List<Mat> frames = VideoSplitter.getFrames(source, NUM_SPLITTED_FRAMES);
+        frames = BlurDetector.getBestFrames(frames, frames.size() / 2);
+
+        // Extract the receipt from the frames
+        List<Mat> receipts = new ArrayList<>();
+        for (Mat frame : frames) {
+            Mat receipt = edgeDetector.extractBiggestObject(frame);
+            System.out.println(receipt.size());
+
+            // Skew each receipt to make them align
+            // @todo Skew + make sure all images have exactly same size.
+            // @todo How to re-size without loosing perspective?
+            // @todo Maybe pad smaller sizes?
+            receipts.add(receipt);
+        }
+
+        // Merge receipts into super-image
+        return BlurDetector.createImage(receipts, NUM_IMAGE_PIECES);
+    }
+
+}
