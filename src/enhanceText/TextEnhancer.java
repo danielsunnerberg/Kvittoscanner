3126bf68c03f590a17eb242c6e1940f57e63a2c9
gurrabz <gustav_m_o_bergstrom@hotmail.com>
2017-03-02 14:11:33 +0100

    Add documentation to TextEnhancer

--- src/enhanceText/TextEnhancer.java
+++ src/enhanceText/TextEnhancer.java
@@ -5,20 +5,23 @@ import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 /**
+ * Class for trying to enhance text in an image
+ *
  * Created by gustavbergstrom on 2017-02-21.
  */
 public class TextEnhancer {
 
 	/**
+	 * Trying to enhance the text using Gaussian blur
 	 *
-	 * @param imageMat
-	 * @param thresh
-	 * @param maxval
-	 * @param kernelWidth
-	 * @param kernelHeight
-	 * @param sigma
-	 * @param thresholdType
-	 * @return
+	 * @param imageMat The image to enhance text in
+	 * @param thresh Default is 0
+	 * @param maxval Default is 255
+	 * @param kernelWidth Default is 3
+	 * @param kernelHeight Default is 3
+	 * @param sigma Default is 0
+	 * @param thresholdType The type of threshold to be used for converting image to binary
+	 * @return An image where the text is enhanced
 	 */
 	public static Mat enhanceWithGaussianBlur (
 			Mat imageMat, int thresh, int maxval, int kernelWidth, int kernelHeight, int sigma,
@@ -26,10 +29,13 @@ public class TextEnhancer {
 
 		Mat newMat = new Mat();
 
+		// Convert image to gray scale
 		Imgproc.cvtColor(imageMat, newMat, Imgproc.COLOR_BGR2GRAY);
 
+		// Apply Gaussian blur to image
 		Imgproc.GaussianBlur(newMat, newMat, new Size(kernelWidth, kernelHeight), sigma);
 
+		// Convert image to binary using the specified threshold
 		if (thresholdType == ThresholdType.OTSU) {
 			Imgproc.threshold(newMat, newMat, thresh, maxval, Imgproc.THRESH_OTSU);
 		}
@@ -38,23 +44,27 @@ public class TextEnhancer {
 	}
 
 	/**
+	 * Trying to enhance the text using Median blur
 	 *
-	 * @param imageMat
-	 * @param thresh
-	 * @param maxval
-	 * @param ksize
-	 * @param thresholdType
-	 * @return
+	 * @param imageMat The image to enhance text in
+	 * @param thresh Default is 0
+	 * @param maxval Default is 255
+	 * @param ksize Default is 3
+	 * @param thresholdType The type of threshold to be used for converting image to binary
+	 * @return An image where the text is enhanced
 	 */
 	public static Mat enhanceWithMedianBlur (
 			Mat imageMat, int thresh, int maxval, int ksize, ThresholdType thresholdType) {
 
 		Mat newMat = new Mat();
 
+		// Convert image to gray scale
 		Imgproc.cvtColor(imageMat, newMat, Imgproc.COLOR_BGR2GRAY);
 
+		// Apply Median blur to image
 		Imgproc.medianBlur(newMat, newMat, ksize);
 
+		// Convert image to binary using the specified threshold
 		if (thresholdType == ThresholdType.OTSU) {
 			Imgproc.threshold(newMat, newMat, thresh, maxval, Imgproc.THRESH_OTSU);
 		}
