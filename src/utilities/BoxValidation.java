485d7b7ccc99fb2698862fc15da31e80dc5961d2
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2017-03-09 09:52:36 +0100

    Expose and document BoxValidation correctly

--- src/utilities/BoxValidation.java
+++ src/utilities/BoxValidation.java
@@ -9,21 +9,32 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class BoxValidation {
+class BoxValidation {
 
     private static final Logger logger = LogManager.getLogger(EdgeDetector.class);
 
     private final static int CORNER_ANGLE_SLACK = 15;
     private final static int ANGLE_ALPHA = 5;
 
-    public static boolean validateCornerAngles(MatOfPoint2f boundingPolygon) {
-
+    /**
+     * Validates the given box, ensuring that no corner has an invalid
+     * corner.
+     *
+     * @param boundingPolygon box to be validated
+     * @return Whether the box is valid
+     */
+    public boolean validateCornerAngles(MatOfPoint2f boundingPolygon) {
         Point[] points = boundingPolygon.toArray();
+        if (points.length != 4) {
+            logger.error("Invalid polygon supplied. Fix me."); // @todo
+            return false;
+        }
+
         int[][] triangles = {
-                new int[]{ 0, 3, 1 }, // top left
-                new int[]{ 1, 0, 2 }, // bottom left
-                new int[]{ 2, 1, 3 }, // bottom right
-                new int[]{ 3, 0, 2 }, // top right
+            new int[]{0, 3, 1}, // top left
+            new int[]{1, 0, 2}, // bottom left
+            new int[]{2, 1, 3}, // bottom right
+            new int[]{3, 0, 2}, // top right
         };
 
         for (int[] corners : triangles) {
@@ -40,8 +51,8 @@ public class BoxValidation {
         return true;
     }
 
-    //Trying new corner method
-    public static boolean validateCornerAngleSum(MatOfPoint2f boundingPolygon){
+    // @todo Use this instead if we're allowing different perspectives
+    private boolean validateCornerAngleSum(MatOfPoint2f boundingPolygon){
         double angleSumPrimary;
         double angleSumSecondary;
         boolean status = true;
@@ -104,7 +115,7 @@ public class BoxValidation {
         return status;
     }
 
-    private static boolean checkAngleSum(double angleSumPrimary, double angleSumSecondary){
+    private boolean checkAngleSum(double angleSumPrimary, double angleSumSecondary){
         boolean angleStatus = true;
         if (!(angleSumPrimary >= 180 - ANGLE_ALPHA && angleSumPrimary <= 180 + ANGLE_ALPHA)){
             angleStatus = false;
@@ -115,7 +126,16 @@ public class BoxValidation {
         return angleStatus;
     }
 
-    private static double angle(Point p1, Point p2, Point p3) {
+    /**
+     * Calculates the angle formed by the triangle formed by the
+     * given three points.
+     *
+     * @param p1 First point
+     * @param p2 Second point
+     * @param p3 Third point
+     * @return Angle formed by the given triangle
+     */
+    double angle(Point p1, Point p2, Point p3) {
         double p12 = length(p1, p2);
         double p13 = length(p1, p3);
         double p23 = length(p2, p3);
@@ -124,7 +144,14 @@ public class BoxValidation {
         return Math.toDegrees(angle);
     }
 
-    private static double length(Point p1, Point p2) {
+    /**
+     * Gets the length of a line formed by two points.
+     *
+     * @param p1 First point
+     * @param p2 Second point
+     * @return Length of formed line
+     */
+    private double length(Point p1, Point p2) {
         return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2));
     }
 }
