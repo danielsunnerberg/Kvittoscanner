1a5c195669f21a84fd8340f06e26166e32bf9b65
gurrabz <gustav_m_o_bergstrom@hotmail.com>
2017-03-09 15:57:11 +0100

    Update TextEnhancer to use ThresholdUtils

--- src/enhanceText/TextEnhancer.java
+++ src/enhanceText/TextEnhancer.java
@@ -11,64 +11,134 @@ import org.opencv.imgproc.Imgproc;
  */
 public class TextEnhancer {
 
+	public static int THRESH_NONE = Integer.MAX_VALUE;
+
 	/**
-	 * Trying to enhance the text using Gaussian blur
+	 * Enhance text using Gaussian blur and a threshold.
 	 *
-	 * @param imageMat The image to enhance text in
-	 * @param thresh Default is 0
-	 * @param maxval Default is 255
-	 * @param kernelWidth Default is 3
-	 * @param kernelHeight Default is 3
-	 * @param sigma Default is 0
-	 * @param thresholdType The type of threshold to be used for converting image to binary
-	 * @return An image where the text is enhanced
+	 * @param mat The matrix to enhance text in.
+	 * @param kernelWidth The kernel width to apply gaussian blur with. Default is 3.
+	 * @param kernelHeight The kernel height to apply gaussian blur with. Default is 3.
+	 * @param sigma The standard deviation to be used when applying gaussian blue. Default is 0.
+	 * @param threshold The threshold value. Should be between 0 and 255.
+	 *                  This value is only used when otsu is set to false. Otherwise it can be set to anything.
+	 * @param maxval The maximum value to set pixels to. Should be between 0 and 255.
+	 *               This value is only used when thresholdType is set to THRESH_BINARY and THRESH_BINARY_INV.
+	 *               Otherwise it can be set to anything.
+	 * @param grayScale If true, the image is converted to gray scale before applying blur. Must be true if otsu is true.
+	 * @param thresholdType The type of threshold to be applied. Can be one of the following values:
+	 *                      THRESH_BINARY Pixels with a higher value than the threshold are set to maxval.
+	 *                      All other pixels are set to 0.
+	 *                      THRESH_BINARY_INV Pixels with a higher value than the threshold are set to 0.
+	 *                      All other pixels are set to maxval.
+	 *                      THRESH_TRUNC Pixels with a higher value than the threshold are set to the threshold value.
+	 *                      All other pixels are kept as they are.
+	 *                      THRESH_TOZERO Pixels with a higher value than the threshold are kept as they are.
+	 *                      All other pixels are set to 0.
+	 *                      THRESH_TOZERO_INV Pixels with a higher value than the threshold are set to 0.
+	 *                      All other pixels are kept as they are.
+	 *                      THRESH_NONE No threshold is applied.
+	 * @param otsu If false, the given threshold value is used. If true, the threshold is calculated using Otsu's method.
+	 *             This value is only used when thresholdType is not set to THRESH_NONE. Otherwise it can be set to anything.
+	 * @return A matrix with enhanced text.
 	 */
-	public static Mat enhanceWithGaussianBlur (
-			Mat imageMat, int thresh, int maxval, int kernelWidth, int kernelHeight, int sigma,
-			ThresholdType thresholdType) {
+	public static Mat gaussianBlurAndThreshold (
+			Mat mat, int kernelWidth, int kernelHeight, int sigma, int threshold, int maxval,
+			boolean grayScale, int thresholdType, boolean otsu) {
 
-		Mat newMat = new Mat();
+		Mat modifiedMat = new Mat();
 
-		// Convert image to gray scale
-		Imgproc.cvtColor(imageMat, newMat, Imgproc.COLOR_BGR2GRAY);
+		if (grayScale) {
+			// Convert image to gray scale
+			Imgproc.cvtColor(mat, modifiedMat, Imgproc.COLOR_BGR2GRAY);
+		} else if (otsu) {
+			System.out.println("[TextEnhancer] (gaussianBlurAndThreshold): grayScale must be true if otsu is true");
+			return mat;
+		} else {
+			modifiedMat = mat;
+		}
 
 		// Apply Gaussian blur to image
-		Imgproc.GaussianBlur(newMat, newMat, new Size(kernelWidth, kernelHeight), sigma);
+		Imgproc.GaussianBlur(modifiedMat, modifiedMat, new Size(kernelWidth, kernelHeight), sigma);
 
-		// Apply the specified threshold
-		if (thresholdType == ThresholdType.OTSU) {
-			newMat = ThresholdUtils.applyThreshold(newMat, 0, maxval, Imgproc.THRESH_BINARY, true);
+		if (thresholdType != THRESH_NONE) {
+			// Apply threshold to image
+			modifiedMat = ThresholdUtils.applyThreshold(modifiedMat, threshold, maxval, thresholdType, otsu);
 		}
 
-		return newMat;
+		return modifiedMat;
 	}
 
+//	public static Mat gaussianBlurAndAdaptiveThreshold (
+//			Mat mat, int kernelWidth, int kernelHeight, int sigma, int maxval, int adaptiveMethod, int thresholdType, int blockSize) {
+//
+//		Mat modifiedMat = new Mat();
+//
+//		// Convert image to gray scale
+//		Imgproc.cvtColor(mat, modifiedMat, Imgproc.COLOR_BGR2GRAY);
+//
+//		// Apply Gaussian blur to image
+//		Imgproc.GaussianBlur(modifiedMat, modifiedMat, new Size(kernelWidth, kernelHeight), sigma);
+//
+//		if (thresholdType != THRESH_NONE) {
+//			// Apply threshold to image
+//			modifiedMat = ThresholdUtils.applyThreshold(modifiedMat, threshold, maxval, thresholdType);
+//			modifiedMat = ThresholdUtils.applyAdaptiveThreshold(modifiedMat, maxval, adaptiveMethod, thresholdType, blockSize);
+//		}
+//
+//		return modifiedMat;
+//	}
+
 	/**
-	 * Trying to enhance the text using Median blur
+	 * Enhance text using median blur and a threshold.
 	 *
-	 * @param imageMat The image to enhance text in
-	 * @param thresh Default is 0
-	 * @param maxval Default is 255
-	 * @param ksize Default is 3
-	 * @param thresholdType The type of threshold to be used for converting image to binary
-	 * @return An image where the text is enhanced
+	 * @param mat The matrix to enhance text in.
+	 * @param kernelSize The kernel size to apply median blur with. Default is 3.
+	 * @param threshold The threshold value. Should be between 0 and 255.
+	 *                  This value is only used when otsu is set to false. Otherwise it can be set to anything.
+	 * @param maxval The maximum value to set pixels to. Should be between 0 and 255.
+	 *               This value is only used when thresholdType is set to THRESH_BINARY and THRESH_BINARY_INV.
+	 *               Otherwise it can be set to anything.
+	 * @param grayScale If true, the image is converted to gray scale before applying blur. Must be true if otsu is true.
+	 * @param thresholdType The type of threshold to be applied. Can be one of the following values:
+	 *                      THRESH_BINARY Pixels with a higher value than the threshold are set to maxval.
+	 *                      All other pixels are set to 0.
+	 *                      THRESH_BINARY_INV Pixels with a higher value than the threshold are set to 0.
+	 *                      All other pixels are set to maxval.
+	 *                      THRESH_TRUNC Pixels with a higher value than the threshold are set to the threshold value.
+	 *                      All other pixels are kept as they are.
+	 *                      THRESH_TOZERO Pixels with a higher value than the threshold are kept as they are.
+	 *                      All other pixels are set to 0.
+	 *                      THRESH_TOZERO_INV Pixels with a higher value than the threshold are set to 0.
+	 *                      All other pixels are kept as they are.
+	 *                      THRESH_NONE No threshold is applied.
+	 * @param otsu If false, the given threshold value is used. If true, the threshold is calculated using Otsu's method.
+	 *             This value is only used when thresholdType is not set to THRESH_NONE. Otherwise it can be set to anything.
+	 * @return A matrix with enhanced text.
 	 */
-	public static Mat enhanceWithMedianBlur (
-			Mat imageMat, int thresh, int maxval, int ksize, ThresholdType thresholdType) {
+	public static Mat medianBlurAndThreshold (
+			Mat mat, int kernelSize, int threshold, int maxval, boolean grayScale, int thresholdType, boolean otsu) {
 
-		Mat newMat = new Mat();
+		Mat modifiedMat = new Mat();
 
-		// Convert image to gray scale
-		Imgproc.cvtColor(imageMat, newMat, Imgproc.COLOR_BGR2GRAY);
-
-		// Apply Median blur to image
-		Imgproc.medianBlur(newMat, newMat, ksize);
-
-		// Apply the specified threshold
-		if (thresholdType == ThresholdType.OTSU) {
-			newMat = ThresholdUtils.applyThreshold(newMat, 0, maxval, Imgproc.THRESH_BINARY, true);
+		if (grayScale) {
+			// Convert image to gray scale
+			Imgproc.cvtColor(mat, modifiedMat, Imgproc.COLOR_BGR2GRAY);
+		} else if (otsu) {
+			System.out.println("[TextEnhancer] (medianBlurAndThreshold): grayScale must be true if otsu is true");
+			return mat;
+		} else {
+			modifiedMat = mat;
 		}
 
-		return newMat;
+		// Apply Median blur to image
+		Imgproc.medianBlur(modifiedMat, modifiedMat, kernelSize);
+
+		if (thresholdType != THRESH_NONE) {
+			// Apply threshold to image
+			modifiedMat = ThresholdUtils.applyThreshold(modifiedMat, threshold, maxval, thresholdType, otsu);
+		}
+
+		return modifiedMat;
 	}
 }
