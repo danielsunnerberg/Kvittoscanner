7c1892d349ecd6818705884b26d9a933962ab68b
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2017-03-09 10:02:47 +0100

    Add runnable example

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,33 @@
-import com.google.gson.JsonObject;
-import com.google.gson.*;
-import com.sun.javafx.geom.Edge;
-import org.junit.runner.Computer;
 import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
-import utilities.ComputerVision;
 import utilities.EdgeDetector;
-import utilities.VideoSplitter;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     private static EdgeDetector edgeDetector = new EdgeDetector();
-    public static void main(String[] args) {
+    private static ReceiptExtractor receiptExtractor = new ReceiptExtractor();
+
+    public static void main(String[] args) throws IOException {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
+        String videoPath = String.format(
+            "%s\\src\\test\\resources\\videos\\reflection-test.mp4",
+            System.getProperty("user.dir")
+        );
 
+        VideoCapture videoCapture = new VideoCapture(videoPath);
+        boolean detectGlare = true;
+        Mat extractReceipt = receiptExtractor.extractReceipt(videoCapture, detectGlare);
 
+        String outPath = String.format("%s\\Desktop\\frames\\merged.png", System.getProperty("user.home"));
+        Imgcodecs.imwrite(outPath, extractReceipt);
     }
+
 }
