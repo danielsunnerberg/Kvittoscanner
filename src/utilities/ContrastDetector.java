6bce8ba1216ca3184dff066215feb56012ff24fa
magnus-gustaver <gustaver@student.chalmers.se>
2017-03-28 16:22:35 +0200

    Added flashdetect

--- src/utilities/ContrastDetector.java
+++ src/utilities/ContrastDetector.java
@@ -1,7 +1,8 @@
 package utilities;
 
-import org.opencv.core.Mat;
-import org.opencv.core.Size;
+import enhanceImage.ImageEnhancer;
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -36,6 +37,71 @@ public class ContrastDetector {
 
         Size size = source.size();
         return (Collections.max(pixels) - Collections.min(pixels)) / (pixelSum / size.width / size.height);
+    }
+
+
+
+    public int getFlashMinY(Mat receipt){
+        int sigma = getSigma(receipt);
+        List<MatOfPoint> contours = getFlashContours(receipt,sigma);
+        int indexOfSmallestArea = getIndexOfSmallestArea(contours);
+
+        double minY = contours.get(indexOfSmallestArea).get(0,0)[1];
+        for (int i = 0; i<contours.get(indexOfSmallestArea).rows(); i++){
+            minY=Math.min(minY, contours.get(indexOfSmallestArea).get(i,0)[1]);
+        }
+        return (int)minY;
+    }
+
+    public int getFlashMaxY(Mat receipt){
+        int sigma = getSigma(receipt);
+        List<MatOfPoint> contours = getFlashContours(receipt,sigma);
+        int indexOfSmallestArea = getIndexOfSmallestArea(contours);
+
+        double maxY = contours.get(indexOfSmallestArea).get(0,0)[1];
+        for (int i = 0; i<contours.get(indexOfSmallestArea).rows(); i++){
+            maxY=Math.max(maxY, contours.get(indexOfSmallestArea).get(i,0)[1]);
+        }
+        return (int)maxY;
+    }
+
+    private int getSigma(Mat receipt){
+        MatOfDouble median = new MatOfDouble();
+        MatOfDouble std = new MatOfDouble();
+        Core.meanStdDev(receipt, median, std);
+        return (int) std.get(0, 0)[0];
+    }
+
+    private List<MatOfPoint> getFlashContours(Mat receipt, int sigma){
+        Mat out = ImageEnhancer.gaussianBlurAndThreshold(receipt,99,99,sigma,200,255,true, Imgproc.THRESH_TOZERO_INV, false);
+        Mat threshOut = new Mat();
+        threshOut = ImageEnhancer.onlyThreshold(out, false, Imgproc.THRESH_BINARY, 1,255,false);
+        List<MatOfPoint> contours = new ArrayList<>();
+        Imgproc.findContours(threshOut, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+        return contours;
+    }
+
+    private int getIndexOfSmallestArea(List<MatOfPoint> contours) {
+        int index = 0;
+        double[] minArea = new double[contours.size()];
+        for (MatOfPoint contour : contours) {
+            minArea[index] = Imgproc.contourArea(contour);
+            index++;
+        }
+        return findMinIdx(minArea);
+    }
+
+    private int findMinIdx(double[] numbers) {
+        if (numbers == null || numbers.length == 0) return -1;
+        double minVal = numbers[0];
+        int minIdx = 0;
+        for(int idx=1; idx<numbers.length; idx++) {
+            if(numbers[idx] < minVal) {
+                minVal = numbers[idx];
+                minIdx = idx;
+            }
+        }
+        return minIdx;
     }
 
 }
